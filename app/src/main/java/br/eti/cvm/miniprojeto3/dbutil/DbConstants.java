b5472df53df63ac12f420489a64e1defd750877a
Cassius Vinicius de Magalhães <cviniciusm@gmail.com>
2016-11-06 17:38:38 -0200

    Correção do nome do banco de dados.

--- app/src/main/java/br/eti/cvm/miniprojeto3/dbutil/DbConstants.java
+++ app/src/main/java/br/eti/cvm/miniprojeto3/dbutil/DbConstants.java
@@ -4,13 +4,13 @@ import br.eti.cvm.miniprojeto3.entity.Cargo;
 
 public class DbConstants {
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "DbConstants.db";
+    public static final String DATABASE_NAME = "miniprojeto3.db";
 
     public static final String TEXT_TYPE = " TEXT";
     public static final String COMMA_SEP = ",";
     public static final String SQL_CREATE_CARGO =
             "CREATE TABLE " + Cargo.TABLE + " (" +
-                    Cargo._ID + " LONG PRIMARY KEY," +
+                    Cargo._ID + " INTEGER PRIMARY KEY," +
                     Cargo.COLUMN_NOME + TEXT_TYPE + " )";
 
     public static final String SQL_DELETE_CARGO =
